a0b78d2c02653bcdfa7809fcc775ea23c246e794
zxw <you@example.com>
2019-06-05 20:06:07 +0800

    绑定enter事件

--- src/main.js
+++ src/main.js
@@ -13,6 +13,25 @@ Vue.use(ElementUI)
 Vue.prototype.echarts = echarts
 Vue.config.productionTip = false
 
+// 注册一个全局自定义指令 `v-focus`
+Vue.directive('focus', {
+  // 当被绑定的元素插入到 DOM 中时……
+  inserted: function (el, obj) { // 这是需要页面刚加载就能进行聚焦操作使用的钩子函数,可以省略的，视具体需求而定
+    // console.log(obj);
+    if (obj.value) { // 对值进行判断
+      // 聚焦元素
+      el.focus()
+    }
+  },
+  // 当指令所在组件的 VNode 及其子 VNode 全部更新后调用
+  componentUpdated: function (el, obj) { // 这是每当绑定的值发生改变时触发的钩子函数
+    // console.log(obj);  //可以打印看一下
+    if (obj.value) {
+      el.focus()
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="mod-home">
-    Home
+    <el-form ref="form" :model="form" :inline="true" label-width="80px">
+      <el-form-item label="姓名:">
+        <el-input v-model="form.name"
+                  focus-index="0"
+                  v-focus="focusIndex === 0"></el-input>
+      </el-form-item>
+      <el-form-item label="性别:">
+         <el-radio-group v-model="form.sex">
+          <el-radio :label="1" v-focus="focusIndex === 1" focus-index="1">男</el-radio>
+          <el-radio :label="2">女</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="爱好:">
+        <el-checkbox-group v-model="form.interest">
+          <el-checkbox label="足球" v-focus="focusIndex === 2" focus-index="2"></el-checkbox>
+          <el-checkbox label="篮球"></el-checkbox>
+          <el-checkbox label="羽毛球"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="运动频率:">
+        <el-select v-model="form.frequency" placeholder="请选择" v-focus="focusIndex === 3" focus-index="3">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="其他信息:">
+        <el-button type="primary" v-focus="focusIndex === 4" focus-index="4">其他信息填写</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -9,7 +41,73 @@ export default {
   name: 'Home',
   data () {
     return {
-
+      focusIndex: 0, // 用来存放下一个应该聚焦的index值
+      form: {
+        name: '',
+        sex: 1,
+        interest: [],
+        frequency: ''
+      },
+      options: [
+        {
+          label: '从来不',
+          value: 0
+        },
+        {
+          label: '一周一次',
+          value: 1
+        },
+        {
+          label: '一周二次',
+          value: 2
+        },
+        {
+          label: '一周三次',
+          value: 3
+        },
+        {
+          label: '一周四次',
+          value: 4
+        },
+        {
+          label: '一周五次',
+          value: 5
+        },
+        {
+          label: '一周六次',
+          value: 6
+        },
+        {
+          label: '每天',
+          value: 7
+        }
+      ]
+    }
+  },
+  created () {
+    this.bindEvent(this)
+  },
+  methods: {
+    nextFocus (index) {
+      console.log(index, 'index')
+      this.focusIndex = index + 1
+    },
+    bindEvent (_this) {
+      document.onkeydown = function (e) {
+        // var ev = e || window.event
+        console.log(document.activeElement, 'element')
+        _this.findCurrentFocusIndex(document.activeElement)
+      }
+    },
+    // 找到对应的focus-index
+    findCurrentFocusIndex (element) {
+      let index = element.getAttribute('focus-index')
+      if (!index) {
+        element = element.parentNode
+        this.findCurrentFocusIndex(element)
+      } else {
+        this.focusIndex = Number(index) + 1
+      }
     }
   }
 }
