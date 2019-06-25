5444829d34dc6024bd20a4fae685fd0474a12f14
zxw <you@example.com>
2019-06-25 21:02:02 +0800

    enter事件

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,29 +10,39 @@
       </el-form-item>
       <el-form-item label="性别:">
          <el-radio-group v-model="form.sex">
-          <el-radio :label="1" v-focus="focusIndex === 1" focus-index="1" class="focus-element">男</el-radio>
-          <el-radio :label="2">女</el-radio>
+          <el-radio v-for="(item, key) in dict['sex']"
+                    :key="key"
+                    :label="item.value"
+                    v-focus="focusIndex === 1"
+                    focus-index="1"
+                    selected-prop="sex"
+                    class="focus-element">{{ item.label }}</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="爱好:">
         <el-checkbox-group v-model="form.interest">
-          <el-checkbox label="足球" v-focus="focusIndex === 2" focus-index="2" class="focus-element"></el-checkbox>
-          <el-checkbox label="篮球"></el-checkbox>
-          <el-checkbox label="羽毛球"></el-checkbox>
+          <el-checkbox v-for="(item, key) in dict['interest']"
+                       :key="key"
+                       :label="item.value"
+                       v-focus="focusIndex === 2"
+                       focus-index="2"
+                       class="focus-element">{{ item.label }}</el-checkbox>
         </el-checkbox-group>
       </el-form-item>
       <el-form-item label="运动频率:">
         <el-select ref="selectEle" v-model="form.frequency" placeholder="请选择" focus-index="3" class="focus-element">
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
+          <el-option v-for="item in dict['frequency']"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
           </el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="其他信息:">
-        <el-button :autofocus="buttonFocus" focus-index="4" class="focus-element" :class="{ 'focusing' : buttonFocus }">其他信息填写</el-button>
+        <el-button :autofocus="buttonFocus"
+                   focus-index="4"
+                   class="focus-element"
+                   :class="{ 'focusing' : buttonFocus }">其他信息填写</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -50,42 +60,73 @@ export default {
         interest: [],
         frequency: ''
       },
-      options: [
-        {
-          label: '从来不',
-          value: 0
-        },
-        {
-          label: '一周一次',
-          value: 1
-        },
-        {
-          label: '一周二次',
-          value: 2
-        },
-        {
-          label: '一周三次',
-          value: 3
-        },
-        {
-          label: '一周四次',
-          value: 4
-        },
-        {
-          label: '一周五次',
-          value: 5
-        },
-        {
-          label: '一周六次',
-          value: 6
-        },
-        {
-          label: '每天',
-          value: 7
-        }
-      ],
+      // 字典数据
+      dict: {
+        // 性别
+        sex: [
+          {
+            label: '男',
+            value: 1
+          },
+          {
+            label: '女',
+            value: 2
+          }
+        ],
+        // 爱好
+        interest: [
+          {
+            label: '足球',
+            value: 1
+          },
+          {
+            label: '篮球',
+            value: 2
+          },
+          {
+            label: '羽毛球',
+            value: 3
+          }
+        ],
+        // 运动频率
+        frequency: [
+          {
+            label: '从来不',
+            value: 0
+          },
+          {
+            label: '一周一次',
+            value: 1
+          },
+          {
+            label: '一周二次',
+            value: 2
+          },
+          {
+            label: '一周三次',
+            value: 3
+          },
+          {
+            label: '一周四次',
+            value: 4
+          },
+          {
+            label: '一周五次',
+            value: 5
+          },
+          {
+            label: '一周六次',
+            value: 6
+          },
+          {
+            label: '每天',
+            value: 7
+          }
+        ]
+      },
       firstFocus: true, // 第一个聚焦
-      buttonFocus: false // 提交按钮聚焦
+      buttonFocus: false, // 提交按钮聚焦
+      typeDict: ['input', 'raido', 'checkbox', 'select', 'button'] // 表单组件类型
     }
   },
   watch: {
@@ -107,16 +148,62 @@ export default {
     this.bindEvent(this)
   },
   methods: {
-    nextFocus (index) {
-      this.focusIndex = Number(index) + 1
+    // 聚焦到下一个
+    nextFocus () {
+      this.focusIndex++
     },
+    // 水平方向移动
+    moveHorizontal () {
+      switch (this.typeDict[this.focusIndex]) {
+        case 'radio':
+          break
+        case 'checkbox':
+          break
+        case 'select':
+          break
+        case 'button':
+          break
+        default:
+          break
+      }
+    },
+    // 垂直方向移动
+    moveVertical () {
+
+    },
+    // 确认当前聚焦的表单的值
+    confirmCurrent () {
+
+    },
+    // 绑定键盘事件
     bindEvent (_this) {
       document.onkeydown = function (e) {
         let ev = e || window.event
         let keyCode = ev.keyCode
         console.log(document.activeElement, 'element')
-        console.log(keyCode, 'keyCode')
-        _this.findCurrentFocusIndex(document.activeElement, keyCode)
+        // console.log(keyCode, 'keyCode')
+        // _this.findCurrentFocusIndex(document.activeElement, keyCode)
+        switch (keyCode) {
+          case 13: // enter回车
+            // isDoc && this.submit()
+            _this.confirmCurrent() // 确认当前聚焦的值
+            _this.nextFocus() // 聚焦下一个
+            break
+          case 38: // PgUp向上
+            _this.moveVertical() // 垂直方向移动
+            break
+          case 40: // PgDn向下
+            _this.moveVertical() // 垂直方向移动
+            break
+          case 37: // Home向左
+            _this.moveHorizontal() // 水平方向移动
+            break
+          case 39: // End向右
+            _this.moveHorizontal() // 水平方向移动
+            break
+          default:
+            break
+        }
       }
     },
     // 找到对应的focus-index
@@ -129,18 +216,23 @@ export default {
         this.findCurrentFocusIndex(element, keyCode)
       } else {
         // isDoc && first.focus() // 当前是document则返回第一个
-        isDoc && this.submit()
         switch (keyCode) {
           case 13: // enter回车
+            isDoc && this.submit()
+            this.confirmCurrent() // 确认当前聚焦的值
             this.nextFocus(index) // 聚焦下一个
             break
           case 38: // PgUp向上
+            this.moveVertical() // 垂直方向移动
             break
           case 40: // PgDn向下
+            this.moveVertical() // 垂直方向移动
             break
           case 37: // Home向左
+            this.moveHorizontal(element, index) // 水平方向移动
             break
           case 39: // End向右
+            this.moveHorizontal(element, index) // 水平方向移动
             break
           default:
             break
